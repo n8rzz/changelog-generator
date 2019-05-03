92266e0bf230cff28401c980810dcc7a8e8e82ad
Nate Geslin <teamtomkins23@gmail.com>
2019-05-03 09:55:17 -0500

    abstracts hasArg commands * adds to help outputs * breaks out command logic to dedicated files * creates `entry.model` * creates `command.enum`

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const minimist = require('minimist');
+const hasArg = require('./lib/hasArg');
+const hasArgCommand = require('./lib/hasArgCommand');
 const cliHelp = require('./lib/cliHelp');
+const compileCommand = require('./lib/command/compile');
+const entryCommand = require('./lib/command/entry');
+const CommandEnum = require('./lib/types/command.enum');
 
 (function() {
-    cliHelp();
+    const args = minimist(process.argv.slice(2));
+
+    if (hasArg(args, 'h')) {
+        cliHelp(args);
+
+        process.exit();
+    }
 
     // loadOptions
-        // !hasOptions -> generateDefaultOptionsFile
+    // !hasOptions -> generateDefaultOptionsFile
 
+    if (hasArgCommand(args, CommandEnum.meta.map.entry) || hasArg(args, 'e')) {
+        entryCommand(args);
+
+        return;
+    } else if (hasArgCommand(args, CommandEnum.meta.map.compile) || hasArg(args, 'c')) {
+        compileCommand(args);
+
+        return;
+    }
 })();
