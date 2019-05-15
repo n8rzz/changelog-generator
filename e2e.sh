f71c7876946cf0b34ec5d83cc8708e0b0461951d
Nate Geslin <teamtomkins23@gmail.com>
2019-05-14 21:27:37 -0500

    adds a little flair and additional logging info to e2e.sh

--- e2e.sh
+++ e2e.sh
@@ -10,10 +10,30 @@ LIB_DIR=lib/
 
 CONFIG_FILE=.changelog.json
 
+log_block () {
+    color=$1
+    msg=$2
+
+    echo ""
+    echo "--- --- :: ::: ::: ::: :: --- ---"
+    echo ""
+    echo "${color} ${msg}${RESET_COLOR}"
+    echo ""
+    echo "--- --- :: ::: ::: ::: :: --- ---"
+    echo ""
+}
+
+log_line () {
+    color=$1
+    msg=$2
+
+    echo ""
+    echo "--- :: ${color}${msg}${RESET_COLOR} :: ---"
+    echo ""
+}
+
 reset_and_rebuild () {
-    echo ""
-    echo "${YELLOW} Removing artifacts and resetting to a pristine state${RESET_COLOR}"
-    echo ""
+    log_block "${YELLOW}" "Removing artifacts and resetting to a pristine state"
 
     [ ! -d "$ENTRIES_DIR" ] || rm -rf "$ENTRIES_DIR"
     [ ! -d "$LIB_DIR" ] || rm -rf "$LIB_DIR"
@@ -23,28 +43,21 @@ reset_and_rebuild () {
 }
 
 run_help_commands () {
-    echo ""
-    echo "${YELLOW} Testing help command outputs...${RESET_COLOR}"
-    echo ""
+    log_block "${YELLOW}" "Testing help command outputs..."
+    log_block "${YELLOW}" "should display root help doc"
 
-    echo ""
-    echo "${YELLOW} should display root help doc${RESET_COLOR}"
-    echo ""
     node ./index.js -h &&
 
-    echo ""
-    echo "${YELLOW} should show help for init command${RESET_COLOR}"
-    echo ""
+    log_block "${YELLOW}" "should show help for init command"
+
     node ./index.js init -h &&
 
-    echo ""
-    echo "${YELLOW} should show help for entry command${RESET_COLOR}"
-    echo ""
+    log_block "${YELLOW}" "should show help for entry command"
+
     node ./index.js entry -h
 
-    echo ""
-    echo "${YELLOW} should show help for generate command${RESET_COLOR}"
-    echo ""
+    log_block "${YELLOW}" "should show help for generate command"
+
     node ./index.js generate -h
 }
 
@@ -56,29 +69,27 @@ run_help_commands
 reset_and_rebuild
 # should display warning about runnint `init` command firsts
 # should create default `.changelog` dir and `.changelog.json` config
+
+log_block "${YELLOW}" "INIT command"
+
 npm run cmd:init:empty
 
 reset_and_rebuild
 
-echo ""
-echo "${YELLOW} ENTRY commands${RESET_COLOR}"
-echo ""
-# should create default entries dir and config with -p project name
+log_block "${YELLOW}" "ENTRY command"
+
+log_line "${GREEN}" "should create default entries dir and config with -p project name"
 npm run cmd:init &&
-# should create an entry prompting user for each input
+log_line "${GREEN}" "should create an entry prompting user for each input"
 npm run cmd:entry:empty &&
-# should create entry using cli arg values
+log_line "${GREEN}" "should create entry using cli arg values"
 npm run cmd:entry &&
-# should create entry using same cli arg values an -f
+log_line "${GREEN}" "should create entry using same cli arg values an -f"
 npm run cmd:entry:force &&
 
-echo ""
-echo "${GREEN} ENTRY commands: PASSED${RESET_COLOR}"
-echo ""
 
-echo ""
-echo "${YELLOW} GENERATE commands${RESET_COLOR}"
-echo ""
+log_block "${YELLOW}" "GENERATE command"
+
 
 npm run cmd:generate:empty &&
 npm run cmd:generate &&
